464960829a458cec77389e4f8f24816f7eddd36f
KPZNET <kpznet@outlook.com>
2021-12-20 10:13:50 -0800

    tdev

--- build_model.py
+++ build_model.py
@@ -1,4 +1,3 @@
-#%%
 import json 
 import numpy as np 
 import tensorflow as tf
@@ -178,18 +177,3 @@ def pickle_trainingdata(rdict, labels, lbl_encoder, responses, training_labels_e
                 
 
 
-#%%
-rdict, intent, labels, num_classes, responses, training_labels, training_sentences,lbl_encoder, training_labels_encoded = build_trainingdata()
-pickle_trainingdata(rdict, labels, lbl_encoder, responses, training_labels_encoded, num_classes)
-#vectorized_sentences = vectorize_all_sentences(training_sentences)
-#pickle_vectorized_sentences(vectorized_sentences)
-#%%
-rdict, labels, lbl_encoder, responses, training_labels, num_classes = load_pickles()
-vectorized_sentences = load_vectorized_sentences()
-
-epochs, history, model = __build_vectorized_model(5,num_classes,training_labels_encoded,vectorized_sentences)
-__save_model_to_file(model)
-
-print("Built")
-
-# %%
--- chatbot_constructor.py
+++ chatbot_constructor.py
@@ -1,14 +1,17 @@
 #%%
-from scrub_pythonQA import query_QandA
-from pythonQAtoJSON import convert_sets_to_json
-from build_model import build_A, build_B
+from build_model import *
 
 
 #%%
-query_QandA()
+rdict, intent, labels, num_classes, responses, training_labels, training_sentences,lbl_encoder, training_labels_encoded = build_trainingdata()
+pickle_trainingdata(rdict, labels, lbl_encoder, responses, training_labels_encoded, num_classes)
+#vectorized_sentences = vectorize_all_sentences(training_sentences)
+#pickle_vectorized_sentences(vectorized_sentences)
 #%%
-convert_sets_to_json()
-#%%
-build_A()
-#%%
-build_B()
+rdict, labels, lbl_encoder, responses, training_labels, num_classes = load_pickles()
+vectorized_sentences = load_vectorized_sentences()
+
+epochs, history, model = __build_vectorized_model(5,num_classes,training_labels_encoded,vectorized_sentences)
+__save_model_to_file(model)
+
+print("Built")
