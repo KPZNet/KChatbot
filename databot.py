a875d946bd2d47d90636c3804a511756939e39e8
Kenneth Ceglia <KPZNet@outlook.com>
2021-12-21 17:25:36 -0800

    dvs

--- databot.py
+++ databot.py
@@ -14,6 +14,29 @@ import build_model
 from build_model import vectorize_all_sentences
 from build_model import load_pickles
 
+from sentence_transformers import SentenceTransformer
+sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+
+from nltk.tokenize import word_tokenize
+  
+
+def compare(p1, p2):
+    u = sbert_model.encode(p1)[0]
+    v = sbert_model.encode(p2)[0]
+    d = np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+    return d
+
+def findwordmatch(sent, baseword):
+    p = -10.0
+    bestguess = ''
+    tsent = word_tokenize(sent)
+    for s in tsent:
+        dprod = compare([baseword], [s])
+        pnew = max(dprod, p)
+        if( pnew > p):
+            bestguess = s
+            p = pnew
+    return bestguess
 
 def vectorize_input_pythonqa(inp):
     p = build_model.vectorize_all_sentences( [inp] )
@@ -43,6 +66,11 @@ def chat(model_name):
         if len(c) == 0:
             print("chatbot does not understand")
         else:
+
+            if tag == 'opendata':
+                bm = findwordmatch(inp,'file')
+                print('DATAFILE open request: MATCH {0}'.format(bm))
+
             print(Fore.LIGHTMAGENTA_EX + "\tTAG:" + Style.RESET_ALL , t)
             print(Fore.LIGHTMAGENTA_EX + "\tPropability:" + Style.RESET_ALL , prob)
             print(Fore.LIGHTMAGENTA_EX + "\tMatched:" + Style.RESET_ALL , patterns[0])
